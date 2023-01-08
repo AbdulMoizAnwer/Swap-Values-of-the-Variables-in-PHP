f1ca1caaadc568aef73b7fdf06976a41cb4c0fab
Abdul Moiz Anwer <89318559+AbdulMoizAnwer@users.noreply.github.com>
2023-01-08 14:14:43 +0500

    Update index23.php

--- index23.php
+++ index23.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Swipe Values of the Variables</title>
+    <title>Swap Values of the Variables</title>
 </head>
 <body>
-    <h1>Write a program to Swipe the Value of the Variables</h1>
+    <h1>Write a program to Swap the Value of the Variables</h1>
     <form action="#" method="POST">
         <label>Enter Value for the Variable "A"</label>
         <input type="text" name="n1">
@@ -19,16 +19,19 @@
 </html>
 
 <?php
+echo "<br>";
+echo "The first way to SWAP.";
+echo "<br>";
 if(isset($_POST['submit'])){
     $A = $_POST['n1'];
     $B = $_POST['n2'];
-    echo "The Values of the Variables A and B before SWIPE are:";
+    echo "The Values of the Variables A and B before SWAP are:";
     echo "<br>";
     echo "Value for the Variable 'A': " . $A;
     echo "<br>";
     echo "Value for the Variable 'B': " . $B;
     echo "<br>";
-    echo "The Values of the Variables A and B after SWIPE are:";
+    echo "The Values of the Variables A and B after SWAP are:";
     echo "<br>";
     $A = $A + $B;
     $B = $A - $B;
@@ -37,4 +40,37 @@ if(isset($_POST['submit'])){
     echo "<br>";
     echo "Value for the Variable 'B': " . $B;
     }
+
+echo "<br>";
+echo "<br>";
+echo "The second way to SWAP.";
+echo "<br>";
+
+$a = 15;
+$b = 27;
+
+// Swap Logic
+
+echo "\nThe number before swapping is:\n";
+echo "Number a =".$a." and b=".$b;
+
+$temp = $a;
+$a = $b;
+$b = $temp;
+
+echo "<br>";
+echo "\nThe number after swapping is: \n";
+echo "Number a =".$a." and b=".$b."\n";
+echo "<br>";
+echo "<br>";
+
+
+echo "The third way to SWAP.";
+echo "<br>";
+$a = 15;
+$b = 276;
+echo "\nBefore swapping:  ". $a . ',' . $b;
+list($a, $b) = array($b, $a);
+echo "<br>";
+echo "\nAfter swapping:  ". $a . ',' . $b."\n";
 ?>
